b36513e979de84a014daec883f4bdd3426525242
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-05-30 21:09:50 -0300

    Migrations

---
+++ database/migrations/20220524145703-Categorie.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'id',
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'name',
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories');
+  }
+};
---
+++ database/migrations/20220524145928-BlogPost.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'id',
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'title',
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'content',
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'userId',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
